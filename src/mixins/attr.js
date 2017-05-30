f6419ae8cd5f97c601314d6f6b8b772158bd47e3
Adam Haile <adam@concretedata.com>
2017-05-29 21:54:59 -0400

    calve off mixins

--- src/mixins/attr.js
+++
@@ -1,7 +0,0 @@
-define('Surplus.attr', ['Surplus'], function (Surplus) {
-    Surplus.attr = function attr(name, value) {
-        return function attr(node) {
-            node.setAttribute(name, value);
-        };
-    };
-});
