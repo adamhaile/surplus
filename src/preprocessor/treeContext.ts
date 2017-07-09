e2b4e55df435bb0dec9c666be799e316dc898c34
Adam Haile <adam.haile@gmail.com>
2017-07-08 23:21:18 -0400

    preserving treeContext before deletion

--- src/preprocessor/treeContext.ts
+++ src/preprocessor/treeContext.ts
@@ -4,28 +4,28 @@ export abstract class Context<T> {
         public readonly node : T
     ) { }
 
-    child<C>(node : C) { return new Child<C, T>(node, this); }
-    sibling<C, S>(node : C, i : number, siblings : S[]) { return new Sibling<C, S, T>(node, i, siblings, this); }
+    child<C>(node : C) { return new ChildContext<C, T>(node, this); }
+    sibling<C, S>(node : C, i : number, siblings : S[]) { return new SiblingContext<C, S, T>(node, i, siblings, this); }
 }
 
-export class Root<T> extends Context<T> { }
+export class RootContext<T> extends Context<T> { }
 
-export class Child<T, P = any> extends Context<T> {
+export class ChildContext<T, P = any> extends Context<T> {
     constructor(
         node : T,
         public readonly parent : Context<P>
     ) { super(node); }
-    swap<U>(node : U) { return new Child(node, this.parent); }
+    swap<U>(node : U) { return new ChildContext(node, this.parent); }
 }
 
-export class Sibling<T, S = any, P = any> extends Context<T> {
+export class SiblingContext<T, S = any, P = any> extends ChildContext<T> {
     constructor(
         node : T,
         public readonly index : number,
         public readonly siblings : S[],
-        public readonly parent : Context<P>
-    ) { super(node); }
-    swap<U>(node : U) { return new Sibling(node, this.index, this.siblings, this.parent); }
+        parent : Context<P>
+    ) { super(node, parent); }
+    swap<U>(node : U) { return new SiblingContext(node, this.index, this.siblings, this.parent); }
 }
     
     
