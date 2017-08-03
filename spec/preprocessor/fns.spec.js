517d5f98e34fb88fb24dd21416617a1638402b44
Adam Haile <adam.haile@gmail.com>
2017-08-02 20:50:54 -0400

    rename mixin.spec.js to fns.spec.js

--- spec/preprocessor/fns.spec.js
+++ spec/preprocessor/fns.spec.js
@@ -1,24 +1,40 @@
-describe("JSX ...mixin function", function () {
+describe("JSX fn", function () {
     var nodeSpy = jasmine.createSpy(),
         argsSpy = jasmine.createSpy().and.returnValue(nodeSpy),
         test = argsSpy;
 
     it("is called with its args then the node", function () {
-        eval(window.SurplusPreprocessor.preprocess('            \
-            argsSpy.calls.reset(), nodeSpy.calls.reset();       \
-                                                                \
-            var a = <a {...test("foo", 2)} />;                  \
-                                                                \
-            expect(argsSpy).toHaveBeenCalledWith("foo", 2);     \
-            expect(nodeSpy).toHaveBeenCalledWith(a, undefined); \
+        eval(window.SurplusPreprocessor.preprocess('            \n\
+            argsSpy.calls.reset(), nodeSpy.calls.reset();       \n\
+                                                                \n\
+            var a = <a fn={test("foo", 2)} />;                  \n\
+                                                                \n\
+            expect(argsSpy).toHaveBeenCalledWith("foo", 2);     \n\
+            expect(nodeSpy).toHaveBeenCalledWith(a, undefined); \n\
         '));
+    });
+
+    it("can pass state back to itself", function () {
+        var code = window.SurplusPreprocessor.preprocess('            \n\
+            var flag = S.data(true),                            \n\
+                mixin = (el, state) =>                          \n\
+                    (flag(), el.id = (state || 0) + 1);         \n\
+                                                                \n\
+            var a = <a fn={mixin} />;                           \n\
+                                                                \n\
+            expect(a.id).toBe("1");                             \n\
+            flag(true);                                         \n\
+            expect(a.id).toBe("2");                             \n\
+        ');
+
+        eval(code);
     });
 
     it("can set properties on the node", function () {
         eval(window.SurplusPreprocessor.preprocess('            \n\
             var mixin = el => el.id = "id";                     \n\
                                                                 \n\
-            var a = <a {...mixin} />;                           \n\
+            var a = <a fn={mixin} />;                           \n\
                                                                 \n\
             expect(a.id).toBe("id");                            \n\
         '));
@@ -28,7 +44,7 @@ describe("JSX ...mixin function", function () {
         eval(window.SurplusPreprocessor.preprocess('            \n\
             var mixin = el => el.id = "foo";                    \n\
                                                                 \n\
-            var a = <a {...mixin} id="bar" />;                  \n\
+            var a = <a fn={mixin} id="bar" />;                  \n\
                                                                 \n\
             expect(a.id).toBe("bar");                           \n\
         '));
@@ -39,7 +55,7 @@ describe("JSX ...mixin function", function () {
             var mixin1 = el => el.id = "foo",                   \n\
                 mixin2 = el => el.id = "bar";                   \n\
                                                                 \n\
-            var a = <a {...mixin1} {...mixin2} />;              \n\
+            var a = <a fn={mixin1} fn={mixin2} />;              \n\
                                                                 \n\
             expect(a.id).toBe("bar");                           \n\
         '));
@@ -50,7 +66,7 @@ describe("JSX ...mixin function", function () {
             var id = S.data("foo"),                             \n\
                 mixin = el => el.id = id();                     \n\
                                                                 \n\
-            var a = <a {...mixin} id="bar" />;                  \n\
+            var a = <a fn={mixin} id="bar" />;                  \n\
                                                                 \n\
             expect(a.id).toBe("bar");                           \n\
             id("bleck");                                        \n\
@@ -64,7 +80,7 @@ describe("JSX ...mixin function", function () {
                 mixin1 = el => el.id = id(),                    \n\
                 mixin2 = el => el.id = "bar";                   \n\
                                                                 \n\
-            var a = <a {...mixin1} {...mixin2} />;              \n\
+            var a = <a fn={mixin1} fn={mixin2} />;              \n\
                                                                 \n\
             expect(a.id).toBe("bar");                           \n\
             id("bleck");                                        \n\
